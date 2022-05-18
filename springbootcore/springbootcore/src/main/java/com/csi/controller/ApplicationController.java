9bd98cc6eb1acc92937d44ff0e4253db7a2ea3fd
Akshay Sanap <you@example.com>
2022-05-18 12:56:38 +0530

    intial commit

---
+++ springbootcore/springbootcore/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,21 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ApplicationController {
+
+    @GetMapping
+    public String sayHello(){
+        return "Welcome To CSI Pune";
+    }
+    @GetMapping("/services")
+    public String sevices(){
+        return "Software Development";
+    }
+
+}
+
